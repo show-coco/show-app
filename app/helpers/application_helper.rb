c3186b34c47a42d443dc7d86d45ad3bda40d3855
show <show75@ezweb.ne.jp>
2019-12-11 16:55:20 +0900

    Modify title +

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(s='')
+        title = "Show_App"
+        if s.empty?
+            title
+        else
+            "#{s} | #{title}"
+        end
+    end
 end
