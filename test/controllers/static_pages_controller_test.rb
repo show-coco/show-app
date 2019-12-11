ffb6f6bc20d3d2099e3ec2486be8cc98034b30c6
show <show75@ezweb.ne.jp>
2019-12-11 16:59:59 +0900

    Refecter Home title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Show_App"
+    assert_select "title", "Show_App"
   end
 
   test "should get help" do
